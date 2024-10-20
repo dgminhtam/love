6fa003e56de394306d7764ca2830c460d990dbfd
Boneiiii <35762319+Boneiiii@users.noreply.github.com>
2024-10-20 19:06:52 +0700

    crate
    create

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    lines: [
+        "Nhân dịp 20/10!",
+        "Gửi một nửa của anh",
+        "Luôn hạnh phúc, xinh đẹp và ngập tràn niềm vui!",
+        "8386",
+        "Iuuu bé nhắmmmm <3",
+    ]
+}
